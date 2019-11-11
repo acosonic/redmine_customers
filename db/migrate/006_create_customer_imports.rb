c7cea39bcfcb76c608bb14dbc9086ddcdc9568cb
bilel-kedidi <bilel.kedidi@gmail.com>
2019-11-11 15:28:10 +0100

    sync

--- db/migrate/006_create_customer_imports.rb
+++ db/migrate/006_create_customer_imports.rb
@@ -1,4 +1,4 @@
-class CreateCustomerImports < ActiveRecord::Migration
+class CreateCustomerImports < Rails.version < '5.1' ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     create_table :customer_imports do |t|
       t.string :url
